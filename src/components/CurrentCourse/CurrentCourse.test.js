383d8f01e495f9bf7ee4897f667bebc1b63dd646
Alexander Bespoyasov <forjob159@gmail.com>
2021-03-27 19:19:58 +0300

    Add a custom render with the redux store provider

--- src/components/CurrentCourse/CurrentCourse.test.js
+++ src/components/CurrentCourse/CurrentCourse.test.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { CurrentCourse } from "./CurrentCourse";
+import { renderWithStore } from "../../testUtils";
 
 describe("when rendered with a course value", () => {
   it("should contain the value in the body", () => {
-    render(<CurrentCourse />);
+    const stateMock = { course: { value: 100500 } };
+    renderWithStore(<CurrentCourse />, { state: stateMock });
     expect(screen.getByText(/100500₽/)).toBeInTheDocument();
   });
 });
---
+++ src/testUtils/index.js
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import course, { initialCourseState } from "../store/features/course/slice";
+
+const reducer = combineReducers({
+  course,
+});
+
+const originalState = {
+  course: initialCourseState,
+};
+
+export function renderWithStore(
+  ui,
+  { state = originalState, dispatch = null } = {}
+) {
+  const middleware = applyMiddleware(thunk);
+  const storeMock = createStore(reducer, state, middleware);
+  return render(<Provider store={storeMock}>{ui}</Provider>);
+}
