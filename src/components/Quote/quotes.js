4ef1a107fe7d64725e211afd68f769b65a8f4ad8
Alexander Bespoyasov <forjob159@gmail.com>
2021-03-27 16:04:08 +0300

    Add a quotes list

---
+++ src/components/Quote/quotes.js
@@ -0,0 +1,14 @@
+export const quotes = [
+  {
+    text: "Работа не волк — в лес не убежит",
+    author: "Народная пословица",
+  },
+  {
+    text: "Откуплюсь по 41 через месяц",
+    author: "Илья Варламов",
+  },
+  {
+    text: "London is the capital of Great Britain",
+    author: "Учитель английского",
+  },
+];
