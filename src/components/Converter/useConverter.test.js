13128a3c03031d7a8d3658b5067dfd72ded5374d
Alexander Bespoyasov <forjob159@gmail.com>
2021-03-27 17:53:17 +0300

    Pan tests for the hook

---
+++ src/components/Converter/useConverter.test.js
@@ -0,0 +1,18 @@
+describe("when rendered", () => {
+  it.todo("RUB input should have a value equal to initial RUB amount");
+  it.todo("USD input should have a value equal to calculated USD amount");
+});
+
+describe("when called an `updateRub` method", () => {
+  it.todo("should update the RUB value");
+  it.todo("should recalculate the USD value");
+});
+
+describe("when called an `updateUsd` method", () => {
+  it.todo("should update the USD value");
+  it.todo("should recalculate the RUB value");
+});
+
+describe("when re-rendered", () => {
+  it.todo("should update its values");
+});
